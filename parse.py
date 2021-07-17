28b9e8c5e541d270c5a631571815923752f7333e
sudo-phantom <sudophantom@gmail.com>
2021-07-16 20:44:30 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,5 +1,5 @@
 from lxml import etree
 
-x = etree.parse('./data/tcp-scan.xml')
+x = etree.parse('./data/vulners-open.xml')
 pretty_xml = etree.tostring(x, pretty_print=True, encoding=str)
 print(pretty_xml)
